f40589b4e2baae3d676deb0cf642126aefd614e3
joaoWiskow <joao.marth@edu.pucrs.br>
2025-10-23 00:17:54 +0000

    adc diagonal incompleta

--- emjava/Rainhas.java
+++ emjava/Rainhas.java
@@ -6,18 +6,18 @@ public class Rainhas{
       Scanner in=new Scanner(System.in);
       System.out.println("Digite o número de rainhas: ");
       int nRainhas=in.nextInt();
-      int tabuleiro[][]=new int[4][4];
-      for(int i=0;i<4;i++){
-       for(int j=0;j<4;j++){
-        tabuleiro[i][j]=0;
-       }
-     }
+      int tabuleiro[][]=new int[8][8];
+    //   for(int i=0;i<8;i++){
+    //    for(int j=0;j<8;j++){
+    //     tabuleiro[i][j]=0;
+    //    }
+    //  }
       preencheLinhas(tabuleiro, 4, 4);
     }
     public static void printa(int nRainhas,int[][] tabuleiro){
      
-     for(int i=0;i<4;i++){
-       for(int j=0;j<4;j++){
+     for(int i=0;i<8;i++){
+       for(int j=0;j<8;j++){
         System.out.print("|"+tabuleiro[i][j]+ "");
        }
       System.out.println("|");
@@ -25,36 +25,37 @@ public class Rainhas{
      
     }
     public static int[][] preencheLinhas(int[][] tabuleiro,int i,int j){
-      int iInf;
-      int jInf;
-      if(1-i<=1-j){
-         iInf=Math.abs(i-(1-i));
-         jInf=Math.abs(j-(1-i));
-      }else{
-         iInf=Math.abs(i-(1-j));
-         jInf=Math.abs(j-(1-j));
-      }
-      int iSup;
-      int jSup;
-      if(8-i<=8-j){
+     
+     tabuleiro[i][j]=1;
+      // for(int k=1;k<8;k++){//coluna
+      //       if(k!=j){
+      //       tabuleiro[i][k]=-1;
+      //       }else{
+      //         tabuleiro[i][k]=1;  
+      //       }
+      //   }
         
-      }
-      for(int k=0;k<8;k++){//coluna
-            if(k!=j){
-            tabuleiro[i][k]=-1;
-            }else{
-              tabuleiro[i][k]=1;  
-            }
+      //   for(int k=1;k<8;k++){//linha
+      //       if(k!=i){
+      //       tabuleiro[k][j]=-1;
+      //       }else{
+      //         tabuleiro[k][j]=1;  
+      //       }
+      //   }
+        for(int k=1;k<8;k++){
+             if(i-k>=1&&j-k>=1){
+              tabuleiro[i][j]=-1;
+             }
+             if(i-k>=1&&j+k<=8){
+              tabuleiro[i][j]=-1;
+             }
+             if(i+k<=8&&j-k>=1){
+              tabuleiro[i][j]=-1;
+             }
+             if(i+k<=8&&j+k<=8){
+              tabuleiro[i][j]=-1;
+             }
         }
-        
-        for(int k=0;k<8;k++){//linha
-            if(k!=i){
-            tabuleiro[k][j]=-1;
-            }else{
-              tabuleiro[k][j]=1;  
-            }
-        }
-        
         printa(1,tabuleiro);
         return tabuleiro;
     }
